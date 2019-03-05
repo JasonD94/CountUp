a7a12f8b1a1139527732b9fc0b3ec3d07f9969fc
Jason Downing <jason@downing.io>
2019-03-04 23:47:45 -0500

    Added full months/days break down for the counter

--- CountUp.js
+++ CountUp.js
@@ -18,23 +18,27 @@ function countUpFromTime(countUp, id) {
 	var seconds = Math.floor( (timeFrom/1000) % 60 ); 		// Convert ms to seconds and round down
 	var minutes = Math.floor( (timeFrom/1000/60) % 60);		// ms to seconds to minutes & round down
 	var hours = Math.floor ( (timeFrom/(1000*60*60)) % 24);	// ms to seconds to mins to hrs
-	var days = Math.floor ( timeFrom/(1000*60*60*24));		// ms to days
+	var days = Math.floor ( timeFrom/(1000*60*60*24) );		// ms to days
 	
 	// seems like 365.2422 is close enough according to Washington Post ¯\_(?)_/¯
 	var years = Math.floor ( timeFrom/(1000*60*60*24*365.2422));
-	
-	// also let's do years to the nearest 2 decimal places too
-	var floatYears = Number(timeFrom/(1000*60*60*24*365.2422)).toFixed(2);
 	
 	// If years > 0, remove some days so it's X years <365 days etc
 	if (years > 0) {
 		days = Math.floor( days - (365.2422 * years) )
 	}
 	
+	// also let's do years & months to the nearest 2 decimal places too
+	// using the year calc above but in decimal, and days calc but converting to months [roughly]
+	var floatYears = Number(timeFrom/(1000*60*60*24*365.2422)).toFixed(2);
+	var floatMonths = Number( timeFrom/(1000*60*60*24) / (365/12) ).toFixed(2);
+	var floatDays = Number ( timeFrom/(1000*60*60*24) ).toFixed(2);
+	
 	// Floating point years below the full X years Y days etc line. 
-	// This would be something like "0.84 year(s)"
+	// This would be something like "0.84 years"
 	$("#floatYears").text(floatYears);
-	$("#timeRefFloatYears").text("years");
+	$("#floatMonths").text(floatMonths);
+	$("#floatDays").text(floatDays);
 
 	// Disable years/days/hours/etc as they hit 0.
 	if (years != 0) {
