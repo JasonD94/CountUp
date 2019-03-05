9811e5e9a0a062bf0cdc05d7f1ecee5cef39c153
Jason Downing <jason@downing.io>
2019-03-04 23:09:07 -0500

    Added x years to count up display/code

--- CountUp.js
+++ CountUp.js
@@ -23,10 +23,18 @@ function countUpFromTime(countUp, id) {
 	// seems like 365.2422 is close enough according to Washington Post ¯\_(?)_/¯
 	var years = Math.floor ( timeFrom/(1000*60*60*24*365.2422));
 	
+	// also let's do years to the nearest 2 decimal places too
+	var floatYears = Number(timeFrom/(1000*60*60*24*365.2422)).toFixed(2);
+	
 	// If years > 0, remove some days so it's X years <365 days etc
 	if (years > 0) {
 		days = Math.floor( days - (365.2422 * years) )
 	}
+	
+	// Floating point years below the full X years Y days etc line. 
+	// This would be something like "0.84 year(s)"
+	$("#floatYears").text(floatYears);
+	$("#timeRefFloatYears").text("years");
 
 	// Disable years/days/hours/etc as they hit 0.
 	if (years != 0) {
